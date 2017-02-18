24983acd4e3cf46b8d43b789ce0fddc9b7db84d9
gauravmolugu <molugugaurav@gmail.com>
2017-02-18 12:02:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/asd.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'asd.jng'));
+app.get('/ui/a.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'a.jng'));
 });
 app.get('/article-one', function (req, res)
 {
