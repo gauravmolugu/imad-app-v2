9e100f7cee08bc8ecf172ee110217de380962657
gauravmolugu <molugugaurav@gmail.com>
2017-03-04 20:04:37 +1100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,9 @@
 console.log('Loaded!');
 
 var text=document.getElementById('text');
-text.innerHTML="bla bla bla";
+    text.innerHTML="bla bla bla";
+
 var pic=document.getElementById('img');
 pic.onclick=function(){
-    pic.style.marginleft='200px';
+    pic.style.marginleft='100px';
 };
