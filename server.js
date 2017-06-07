3009b14bfbf45f7ff24d65d8431ebef715f9bcdc
gauravmolugu <molugugaurav@gmail.com>
2017-06-07 20:03:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,6 +107,16 @@ app.get('/hash/:input',function(req,res){
     res.send(hashedString);
 });
 
+var pool=new Pool(config);
+
+app.get('/create-user',function(req,res){
+   var salt=crypto.getRandomBytes(128).toString('hex');
+   var dbString=hash(password,salt);
+   pool.query('INSERT INTO "user"(username,password)VALUES($1,$2)',[username,dbString],function(err,result){
+       
+   });
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
