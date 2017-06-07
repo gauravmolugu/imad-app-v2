4ecf003bfb4b5e1696691f83ab1e79b7a852e861
gauravmolugu <molugugaurav@gmail.com>
2017-06-07 19:46:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/', function (req, res) {
 
 function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,100000,512,'sha512');
-    return [pbkdf2,'this_is_random',100000, hashed.toString('hex')];
+    return [pbkdf2,'this_is_random',100000, hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input',function(req,res){
