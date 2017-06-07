960a3a1394fca4d6ce667e0fa751895797239ae6
gauravmolugu <molugugaurav@gmail.com>
2017-06-07 19:28:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto=require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
@@ -94,6 +95,16 @@ app.get('/counter',function(req,res){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,100000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input',function(req,res){
+    var hashedString=hash(req.params.input,'this_is_random');
+    res.send(hashedString);
 });
 
 app.get('/ui/style.css', function (req, res) {
